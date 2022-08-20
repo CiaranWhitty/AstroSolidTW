511c967b5d31838e9cce8f6f8e2fc3cfe5dda5e1
Ciaran Whitty <whittyciaran1998@gmail.com>
2022-08-20 23:25:39 +0100

    Troubleshooting 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 import node from "@astrojs/node";
 
