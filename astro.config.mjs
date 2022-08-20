0a5eed38d189b23e3eddb2a7ceed4669fd9eb3bd
Ciaran Whitty <whittyciaran1998@gmail.com>
2022-08-20 23:16:56 +0100

    Added Daisyui, Added Node For SRR, Added Product list, Added Product Page, Made Nice Nav,

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()]
+  integrations: [solid(), tailwind()],
+  output: "server",
+  adapter: node(),
 });
---
+++ src/data/products.ts
@@ -0,0 +1,752 @@
+export const ProductData = [
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "01",
+    name: "Product 1",
+    url_name: "product_1",
+    price: 100,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "02",
+    name: "Product 2",
+    url_name: "product_2",
+    price: 20,
+    categories: [
+      { name: "Top", url_name: "tops" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "03",
+    name: "Product 3",
+    url_name: "product_3",
+    price: 10,
+    categories: [
+      { name: "Tie", url_name: "ties" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "04",
+    name: "Product 4",
+    url_name: "product_4",
+    price: 65,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+  {
+    uId: "05",
+    name: "Product 5",
+    url_name: "product_5",
+    price: 50,
+    categories: [
+      { name: "Jeans", url_name: "jeans" },
+      { name: "Products", url_name: "products" },
+    ],
+  },
+];
