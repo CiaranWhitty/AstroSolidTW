858fc940b0c279ff6455d38b9a0828dfb457324c
Ciaran Whitty <whittyciaran1998@gmail.com>
2022-08-21 18:12:37 +0100

    Updated Images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
-
 import node from "@astrojs/node";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()],
+  integrations: [solid(), tailwind(), image()],
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
